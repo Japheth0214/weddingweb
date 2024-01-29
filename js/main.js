05ef3cae9ac404c60387673236845dec7887fa1f
Japheth0214 <japarcillians@gmail,com>
2024-01-29 21:06:05 +0800

    Final Version .1

--- js/main.js
+++ js/main.js
@@ -28,6 +28,43 @@
     });
 
 
+    // Function to set a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Function to get a cookie value by name
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// Show error message on the first visit
+$(document).ready(function () {
+    var hasVisitedBefore = getCookie('visitedBefore');
+    if (!hasVisitedBefore) {
+        $('#desktopModeMessage').fadeIn('slow');
+        // Set a cookie to remember the visit
+        setCookie('visitedBefore', true, 365);
+        setTimeout(function () {
+            $('#desktopModeMessage').fadeOut('slow');
+        }, 5000);
+    }
+});
+
+
     // Modal Video
     $(document).ready(function () {
         var $videoSrc;

ed45da135ea5c4bd9459a868aa5a2c3d7a7644e9
Japheth0214 <japarcillians@gmail,com>
2024-02-01 16:59:38 +0800

    Final Version

--- js/main.js
+++ js/main.js
@@ -153,35 +153,36 @@ var countdownInterval = setInterval(function() {
     });
 
 
-    // Gallery carousel
-    $(".gallery-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1500,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            },
-            1200:{
-                items:5
-            }
+   $(".gallery-carousel").owlCarousel({
+    autoplay: true, // Enable automatic sliding
+    autoplayTimeout: 3000, // Set the timeout in milliseconds (e.g., 5000ms for 5 seconds)
+    autoplayHoverPause: true, // Pause on hover
+    smartSpeed: 1500,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        576: {
+            items: 2
+        },
+        768: {
+            items: 3
+        },
+        992: {
+            items: 4
+        },
+        1200: {
+            items: 5
         }
-    });
-    
+    }
+});
+
 })(jQuery);
 
